8355a256069ec20b8f01ae17501b2623bbab85c3
David Bauer <dbauer1@udayton.edu>
2020-05-15 16:27:35 -0400

    Update register file

--- register.php
+++ register.php
@@ -5,5 +5,22 @@ $REGISTER_LTI2 = array(
     "FontAwesome" => "fa-star",
     "short_name" => "Simple Certificate",
     "description" => "A simple app to award a user a certificate when they launch the app.",
-    "messages" => array("launch", "launch_grade")
+    "messages" => array("launch"),
+    "privacy_level" => "public",  // anonymous, name_only, public
+    "license" => "Apache",
+    "languages" => array(
+        "English",
+    ),
+    "source_url" => "https://github.com/udaytonapps/simplecertificate",
+    // For now Tsugi tools delegate this to /lti/store
+    "placements" => array(
+        /*
+        "course_navigation", "homework_submission",
+        "course_home_submission", "editor_button",
+        "link_selection", "migration_selection", "resource_selection",
+        "tool_configuration", "user_navigation"
+        */
+    ),
+    "screen_shots" => array(
+    )
 );
